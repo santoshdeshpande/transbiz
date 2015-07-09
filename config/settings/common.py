2932652a3c3584acdbc6b1f44c71c04f3fe0b211
Santosh Suresh <santoshdeshpande@gmail.com>
2015-07-09 19:00:20 +0530

    * Created initial version of the apiserver app * Added django rest framework

--- config/settings/common.py
+++ config/settings/common.py
@@ -44,6 +44,7 @@ THIRD_PARTY_APPS = (
 # Apps specific for this project go here.
 LOCAL_APPS = (
     'transbiz.users',  # custom users app
+    'transbiz.apiserver',
     # Your stuff: custom apps go here
 )
 
