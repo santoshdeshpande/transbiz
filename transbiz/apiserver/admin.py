6085e2b0cc81dcb13d37235067c2742f4bbe5301
Santosh Suresh <santoshdeshpande@gmail.com>
2015-07-14 12:10:01 +0530

    Support for the industry vertical model - story id 99000856

--- transbiz/apiserver/admin.py
+++ transbiz/apiserver/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from transbiz.apiserver.models import State
+from transbiz.apiserver.models import State, IndustryVertical
 
 admin.site.register(State)
+admin.site.register(IndustryVertical)
--- transbiz/apiserver/models.py
+++ transbiz/apiserver/models.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+from model_utils.models import TimeStampedModel
 
 
-class State(models.Model):
+class State(TimeStampedModel):
 
     name = models.CharField(max_length=20, unique=True, verbose_name='Name of the State')
     short_name = models.CharField(max_length=2, verbose_name='Short Name for the State(KA, MH etc)')
@@ -12,4 +13,12 @@ class State(models.Model):
     def __unicode__(self):
         return self.name
 
+
+class IndustryVertical(TimeStampedModel):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.name
 
