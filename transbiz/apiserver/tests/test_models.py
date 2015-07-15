b5c5df606cda63a257e30b24c12eed26aed87917
Santosh Suresh <santoshdeshpande@gmail.com>
2015-07-15 07:59:25 +0530

    Added more tests for company and also ensured that the state is set to the city chosen

--- transbiz/apiserver/tests/test_models.py
+++ transbiz/apiserver/tests/test_models.py
@@ -79,11 +79,33 @@ class TestSubscriptionPlan(unittest.TestCase):
 
 class TestCompanyModel(unittest.TestCase):
     def setUp(self):
-        state,result = State.objects.get_or_create(name='Karnataka', short_name='KA')
-        self.state = state
+        self.state,result = State.objects.get_or_create(name='Karnataka', short_name='KA')
         self.city,result = City.objects.get_or_create(name='Bangalore', state=self.state)
         self.default_company = Company(name='Apple', address_line_1='Somewhere',city=self.city, state=self.state, pin_code='560010',landline_number='080-35353534',tin='1111111',tan='111111111',service_tax_number='11111111',)
 
     def test_a_valid_company_is_saved(self):
         self.default_company.save()
         self.assertEqual(self.default_company.name,'Apple')
+
+    def test_company_name_should_be_unique(self):
+        with self.assertRaises(IntegrityError):
+            self.default_company.save()
+            company_another = Company(name='Apple', address_line_1='Somewhere',city=self.city, state=self.state, pin_code='560010',landline_number='080-35353534',tin='1111111',tan='111111111',service_tax_number='11111111',)
+            company_another.save()
+
+    def test_not_valid_if_address_1_is_missing(self):
+        with self.assertRaises(IntegrityError):
+            self.default_company.address_line_1 = None
+            self.default_company.save()
+
+    def test_not_valid_if_city_state_are_missing(self):
+        with self.assertRaises(ValueError):
+            company_another = Company(name='Apple1', address_line_1='Somewhere',city=None, state=None, pin_code='560010',landline_number='080-35353534',tin='1111111',tan='111111111',service_tax_number='11111111',)
+            company_another.save()
+
+    def test_state_is_set_based_on_the_city_chosen(self):
+        another_state,result = State.objects.get_or_create(name='Maharashtra', short_name='MH')
+        company_another = Company(name='Apple2', address_line_1='Somewhere',city=self.city, state=another_state, pin_code='560010',landline_number='080-35353534',tin='1111111',tan='111111111',service_tax_number='11111111',)
+        company_another.save()
+        self.assertEqual(company_another.state.short_name, 'KA')
+
